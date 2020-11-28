bb9caf59c4fdbf5ad930ba98ba39e333e83ac77f
bojan <bojan.petrovic.2745@outlook.com>
2020-11-28 13:57:42 +0100

    Add project files.

---
+++ CS322-ASP.NET-MVC/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using CS322_ASP.NET_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CS322_ASP.NET_MVC.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
